8cc062caf96f574f6073eb01cb5c6551e0af9428
Edinei Piovesan da Silva <edinei.silva@prestadores.uhgbrasil.com.br>
2019-04-18 13:25:04 -0300

    Swap 'actual' and 'expected' from assertEquals.

--- src/main/java/com/edn/TVMazeApiResponse.java
+++ src/main/java/com/edn/TVMazeApiResponse.java
@@ -3,17 +3,20 @@ package com.edn;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class TVMazeApiResponse {
 
     @SerializedName("score")
     @Expose
     private Double score;
+
     @SerializedName("show")
     @Expose
     private Show show;
@@ -21,60 +24,78 @@ public class TVMazeApiResponse {
     @Getter
     @Setter
     public static class Show {
+
         @SerializedName("id")
         @Expose
         private Integer id;
+
         @SerializedName("url")
         @Expose
         private String url;
+
         @SerializedName("name")
         @Expose
         private String name;
+
         @SerializedName("type")
         @Expose
         private String type;
+
         @SerializedName("language")
         @Expose
         private String language;
+
         @SerializedName("genres")
         @Expose
         private List<String> genres = null;
+
         @SerializedName("status")
         @Expose
         private String status;
+
         @SerializedName("runtime")
         @Expose
         private Integer runtime;
+
         @SerializedName("premiered")
         @Expose
         private String premiered;
+
         @SerializedName("officialSite")
         @Expose
         private Object officialSite;
+
         @SerializedName("schedule")
         @Expose
         private Schedule schedule;
         @SerializedName("rating")
         @Expose
         private Rating rating;
+
         @SerializedName("weight")
         @Expose
         private Integer weight;
+
         @SerializedName("network")
         @Expose
         private Network network;
+
         @SerializedName("webChannel")
         @Expose
         private Object webChannel;
+
         @SerializedName("externals")
         @Expose
         private Externals externals;
+
         @SerializedName("image")
         @Expose
         private Image image;
+
         @SerializedName("summary")
         @Expose
         private String summary;
+
         @SerializedName("updated")
         @Expose
         private Integer updated;
@@ -94,11 +115,11 @@ public class TVMazeApiResponse {
 
         @SerializedName("code")
         @Expose
+
         private String code;
         @SerializedName("timezone")
         @Expose
         private String timezone;
-
     }
 
     @Getter
@@ -116,21 +137,19 @@ public class TVMazeApiResponse {
         @SerializedName("imdb")
         @Expose
         private String imdb;
-
     }
 
     @Getter
     @Setter
     public static class Image {
 
-
         @SerializedName("medium")
         @Expose
         private String medium;
+
         @SerializedName("original")
         @Expose
         private String original;
-
     }
 
     @Getter
@@ -144,10 +163,10 @@ public class TVMazeApiResponse {
         @SerializedName("previousepisode")
         @Expose
         private Previousepisode previousepisode;
+
         @SerializedName("nextepisode")
         @Expose
         private Nextepisode nextepisode;
-
     }
 
     @Getter
@@ -161,10 +180,10 @@ public class TVMazeApiResponse {
         @SerializedName("name")
         @Expose
         private String name;
+
         @SerializedName("country")
         @Expose
         private Country country;
-
     }
 
     @Getter
@@ -174,7 +193,6 @@ public class TVMazeApiResponse {
         @SerializedName("href")
         @Expose
         private String href;
-
     }
 
     @Getter
@@ -184,7 +202,6 @@ public class TVMazeApiResponse {
         @SerializedName("href")
         @Expose
         private String href;
-
     }
 
     @Getter
@@ -194,21 +211,19 @@ public class TVMazeApiResponse {
         @SerializedName("average")
         @Expose
         private Double average;
-
     }
 
     @Getter
     @Setter
     public static class Schedule {
 
-
         @SerializedName("time")
         @Expose
         private String time;
+
         @SerializedName("days")
         @Expose
         private List<String> days = null;
-
     }
 
     @Getter
@@ -218,6 +233,5 @@ public class TVMazeApiResponse {
         @SerializedName("href")
         @Expose
         private String href;
-
     }
 }
--- src/test/java/com/edn/GsonTest.java
+++ src/test/java/com/edn/GsonTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
@@ -19,45 +21,58 @@ public class GsonTest {
         Type collectionType = new TypeToken<Collection<TVMazeApiResponse>>(){}.getType();
         List<TVMazeApiResponse> response = new Gson().fromJson(Constants.TV_MAZE_JSON_RESPONSE, collectionType);
 
-        TVMazeApiResponse firstResponse = response.get(0);
+        TVMazeApiResponse firstResponse = response.stream().findFirst().orElse(new TVMazeApiResponse());
         assertEquals(firstResponse.getScore(), (Double) 17.677668);
 
         TVMazeApiResponse.Show show = firstResponse.getShow();
-        assertEquals(show.getId(), (Integer) 41734);
-        assertEquals(show.getUrl(), "http://www.tvmaze.com/shows/41734/girls");
-        assertEquals(show.getName(), "GIRLS");
-        assertEquals(show.getType(), "Scripted");
-        assertEquals(show.getLanguage(), "Mongolian");
-        assertEquals(show.getGenres(), singletonList("Comedy"));
-        assertEquals(show.getStatus(), "Running");
-        assertEquals(show.getRuntime(), (Integer) 41);
+        assertEquals((Integer) 41734, show.getId());
+        assertEquals("http://www.tvmaze.com/shows/41734/girls", show.getUrl());
+        assertEquals("GIRLS", show.getName());
+        assertEquals("Scripted", show.getType());
+        assertEquals("Mongolian", show.getLanguage());
+        assertEquals(singletonList("Comedy"), show.getGenres());
+        assertEquals("Running", show.getStatus());
+        assertEquals((Integer) 41, show.getRuntime());
         assertNull(show.getPremiered());
         assertNull(show.getOfficialSite());
         assertNull(show.getRating().getAverage());
-        assertEquals(show.getWeight(), (Integer) 0);
+        assertEquals((Integer) 0, show.getWeight());
         assertNull(show.getWebChannel());
         assertNull(show.getSummary());
-        assertEquals(show.getUpdated(), (Integer) 1555040569);
-        assertEquals(show.getLinks().getSelf().getHref(), "http://api.tvmaze.com/shows/41734");
+        assertEquals((Integer) 1555040569, show.getUpdated());
+        assertEquals("http://api.tvmaze.com/shows/41734", show.getLinks().getSelf().getHref());
 
         TVMazeApiResponse.Schedule schedule = show.getSchedule();
-        assertEquals(schedule.getTime(), "");
-        assertEquals(schedule.getDays(), singletonList("Thursday"));
+        assertEquals("", schedule.getTime());
+        assertEquals(singletonList("Thursday"), schedule.getDays());
 
         TVMazeApiResponse.Network network = show.getNetwork();
-        assertEquals(network.getId(), (Integer) 1672);
-        assertEquals(network.getName(), "UBS");
-        assertEquals(network.getCountry().getName(), "Mongolia");
-        assertEquals(network.getCountry().getCode(), "MN");
-        assertEquals(network.getCountry().getTimezone(), "Asia/Ulaanbaatar");
+        assertEquals((Integer) 1672, network.getId());
+        assertEquals("UBS", network.getName());
+        assertEquals("Mongolia", network.getCountry().getName());
+        assertEquals("MN", network.getCountry().getCode());
+        assertEquals("Asia/Ulaanbaatar", network.getCountry().getTimezone());
 
         TVMazeApiResponse.Externals externals = show.getExternals();
         assertNull(externals.getTvrage());
         assertNull(externals.getThetvdb());
-        assertEquals(externals.getImdb(), "tt8709752");
+        assertEquals("tt8709752", externals.getImdb());
 
         TVMazeApiResponse.Image image = show.getImage();
-        assertEquals(image.getMedium(), "http://static.tvmaze.com/uploads/images/medium_portrait/191/478539.jpg");
-        assertEquals(image.getOriginal(), "http://static.tvmaze.com/uploads/images/original_untouched/191/478539.jpg");
+        assertEquals("http://static.tvmaze.com/uploads/images/medium_portrait/191/478539.jpg", image.getMedium());
+        assertEquals("http://static.tvmaze.com/uploads/images/original_untouched/191/478539.jpg", image.getOriginal());
+    }
+
+    @Test
+    public void deserializeAsMap() {
+        Type collectionType = new TypeToken<Collection<TVMazeApiResponse>>() {
+        }.getType();
+        List<TVMazeApiResponse> response = new Gson().fromJson(Constants.TV_MAZE_JSON_RESPONSE, collectionType);
+
+        Map<Integer, TVMazeApiResponse.Show> showMap = new HashMap<>();
+        response.forEach(r -> showMap.put(r.getShow().getId(), r.getShow()));
+
+        assertEquals("Girls", showMap.get(139).getName());
+        assertEquals("Good Girls", showMap.get(23542).getName());
     }
 }
