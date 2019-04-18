05dede84f13e20e3c2899bf5f85e3976d8428d78
Edinei Piovesan da Silva <edinei.silva@prestadores.uhgbrasil.com.br>
2019-04-18 10:03:02 -0300

    Inner classes

--- src/main/java/com/edn/TVMazeApiResponse.java
+++ src/main/java/com/edn/TVMazeApiResponse.java
@@ -17,208 +17,207 @@ public class TVMazeApiResponse {
     @SerializedName("show")
     @Expose
     private Show show;
-}
 
-@Getter
-@Setter
-class Show {
+    @Getter
+    @Setter
+    public static class Show {
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("url")
+        @Expose
+        private String url;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("type")
+        @Expose
+        private String type;
+        @SerializedName("language")
+        @Expose
+        private String language;
+        @SerializedName("genres")
+        @Expose
+        private List<String> genres = null;
+        @SerializedName("status")
+        @Expose
+        private String status;
+        @SerializedName("runtime")
+        @Expose
+        private Integer runtime;
+        @SerializedName("premiered")
+        @Expose
+        private String premiered;
+        @SerializedName("officialSite")
+        @Expose
+        private Object officialSite;
+        @SerializedName("schedule")
+        @Expose
+        private Schedule schedule;
+        @SerializedName("rating")
+        @Expose
+        private Rating rating;
+        @SerializedName("weight")
+        @Expose
+        private Integer weight;
+        @SerializedName("network")
+        @Expose
+        private Network network;
+        @SerializedName("webChannel")
+        @Expose
+        private Object webChannel;
+        @SerializedName("externals")
+        @Expose
+        private Externals externals;
+        @SerializedName("image")
+        @Expose
+        private Image image;
+        @SerializedName("summary")
+        @Expose
+        private String summary;
+        @SerializedName("updated")
+        @Expose
+        private Integer updated;
 
-    @SerializedName("id")
-    @Expose
-    private Integer id;
-    @SerializedName("url")
-    @Expose
-    private String url;
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("type")
-    @Expose
-    private String type;
-    @SerializedName("language")
-    @Expose
-    private String language;
-    @SerializedName("genres")
-    @Expose
-    private List<String> genres = null;
-    @SerializedName("status")
-    @Expose
-    private String status;
-    @SerializedName("runtime")
-    @Expose
-    private Integer runtime;
-    @SerializedName("premiered")
-    @Expose
-    private String premiered;
-    @SerializedName("officialSite")
-    @Expose
-    private Object officialSite;
-    @SerializedName("schedule")
-    @Expose
-    private Schedule schedule;
-    @SerializedName("rating")
-    @Expose
-    private Rating rating;
-    @SerializedName("weight")
-    @Expose
-    private Integer weight;
-    @SerializedName("network")
-    @Expose
-    private Network network;
-    @SerializedName("webChannel")
-    @Expose
-    private Object webChannel;
-    @SerializedName("externals")
-    @Expose
-    private Externals externals;
-    @SerializedName("image")
-    @Expose
-    private Image image;
-    @SerializedName("summary")
-    @Expose
-    private String summary;
-    @SerializedName("updated")
-    @Expose
-    private Integer updated;
+        @SerializedName("_links")
+        @Expose
+        private Links links;
+    }
 
-    @SerializedName("_links")
-    @Expose
-    private Links links;
-}
+    @Getter
+    @Setter
+    public static class Country {
 
-@Getter
-@Setter
-class Country {
+        @SerializedName("name")
+        @Expose
+        private String name;
 
-    @SerializedName("name")
-    @Expose
-    private String name;
+        @SerializedName("code")
+        @Expose
+        private String code;
+        @SerializedName("timezone")
+        @Expose
+        private String timezone;
 
-    @SerializedName("code")
-    @Expose
-    private String code;
-    @SerializedName("timezone")
-    @Expose
-    private String timezone;
+    }
 
-}
+    @Getter
+    @Setter
+    public static class Externals {
 
-@Getter
-@Setter
-class Externals {
+        @SerializedName("tvrage")
+        @Expose
+        private Integer tvrage;
 
-    @SerializedName("tvrage")
-    @Expose
-    private Integer tvrage;
+        @SerializedName("thetvdb")
+        @Expose
+        private Integer thetvdb;
 
-    @SerializedName("thetvdb")
-    @Expose
-    private Integer thetvdb;
+        @SerializedName("imdb")
+        @Expose
+        private String imdb;
 
-    @SerializedName("imdb")
-    @Expose
-    private String imdb;
+    }
 
-}
-
-@Getter
-@Setter
-class Image {
+    @Getter
+    @Setter
+    public static class Image {
 
 
-    @SerializedName("medium")
-    @Expose
-    private String medium;
-    @SerializedName("original")
-    @Expose
-    private String original;
+        @SerializedName("medium")
+        @Expose
+        private String medium;
+        @SerializedName("original")
+        @Expose
+        private String original;
 
-}
+    }
 
-@Getter
-@Setter
-class Links {
+    @Getter
+    @Setter
+    public static class Links {
 
-    @SerializedName("self")
-    @Expose
-    private Self self;
+        @SerializedName("self")
+        @Expose
+        private Self self;
 
-    @SerializedName("previousepisode")
-    @Expose
-    private Previousepisode previousepisode;
-    @SerializedName("nextepisode")
-    @Expose
-    private Nextepisode nextepisode;
+        @SerializedName("previousepisode")
+        @Expose
+        private Previousepisode previousepisode;
+        @SerializedName("nextepisode")
+        @Expose
+        private Nextepisode nextepisode;
 
-}
+    }
 
-@Getter
-@Setter
-class Network {
+    @Getter
+    @Setter
+    public static class Network {
 
-    @SerializedName("id")
-    @Expose
-    private Integer id;
+        @SerializedName("id")
+        @Expose
+        private Integer id;
 
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("country")
-    @Expose
-    private Country country;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("country")
+        @Expose
+        private Country country;
 
-}
+    }
 
-@Getter
-@Setter
-class Nextepisode {
+    @Getter
+    @Setter
+    public static class Nextepisode {
 
-    @SerializedName("href")
-    @Expose
-    private String href;
+        @SerializedName("href")
+        @Expose
+        private String href;
 
-}
+    }
 
-@Getter
-@Setter
-class Previousepisode {
+    @Getter
+    @Setter
+    public static class Previousepisode {
 
-    @SerializedName("href")
-    @Expose
-    private String href;
+        @SerializedName("href")
+        @Expose
+        private String href;
 
-}
+    }
 
-@Getter
-@Setter
-class Rating {
+    @Getter
+    @Setter
+    public static class Rating {
 
-    @SerializedName("average")
-    @Expose
-    private Double average;
+        @SerializedName("average")
+        @Expose
+        private Double average;
 
-}
+    }
 
-@Getter
-@Setter
-class Schedule {
+    @Getter
+    @Setter
+    public static class Schedule {
 
 
-    @SerializedName("time")
-    @Expose
-    private String time;
-    @SerializedName("days")
-    @Expose
-    private List<String> days = null;
+        @SerializedName("time")
+        @Expose
+        private String time;
+        @SerializedName("days")
+        @Expose
+        private List<String> days = null;
 
-}
+    }
 
-@Getter
-@Setter
-class Self {
+    @Getter
+    @Setter
+    public static class Self {
 
-    @SerializedName("href")
-    @Expose
-    private String href;
+        @SerializedName("href")
+        @Expose
+        private String href;
 
+    }
 }
--- src/test/java/com/edn/GsonTest.java
+++ src/test/java/com/edn/GsonTest.java
@@ -1,15 +1,16 @@
 package com.edn;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
 
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
 
 import static java.util.Collections.singletonList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class GsonTest {
 
@@ -19,35 +20,44 @@ public class GsonTest {
         List<TVMazeApiResponse> response = new Gson().fromJson(Constants.TV_MAZE_JSON_RESPONSE, collectionType);
 
         TVMazeApiResponse firstResponse = response.get(0);
-
         assertEquals(firstResponse.getScore(), (Double) 17.677668);
-        assertEquals(firstResponse.getShow().getId(), (Integer) 41734);
-        assertEquals(firstResponse.getShow().getUrl(), "http://www.tvmaze.com/shows/41734/girls");
-        assertEquals(firstResponse.getShow().getName(), "GIRLS");
-        assertEquals(firstResponse.getShow().getType(), "Scripted");
-        assertEquals(firstResponse.getShow().getLanguage(), "Mongolian");
-        assertEquals(firstResponse.getShow().getGenres(), singletonList("Comedy"));
-        assertEquals(firstResponse.getShow().getStatus(), "Running");
-        assertEquals(firstResponse.getShow().getRuntime(), (Integer) 41);
-        assertNull(firstResponse.getShow().getPremiered());
-        assertNull(firstResponse.getShow().getOfficialSite());
-        assertEquals(firstResponse.getShow().getSchedule().getTime(), "");
-        assertEquals(firstResponse.getShow().getSchedule().getDays(), singletonList("Thursday"));
-        assertNull(firstResponse.getShow().getRating().getAverage());
-        assertEquals(firstResponse.getShow().getWeight(), (Integer) 0);
-        assertEquals(firstResponse.getShow().getNetwork().getId(), (Integer) 1672);
-        assertEquals(firstResponse.getShow().getNetwork().getName(), "UBS");
-        assertEquals(firstResponse.getShow().getNetwork().getCountry().getName(), "Mongolia");
-        assertEquals(firstResponse.getShow().getNetwork().getCountry().getCode(), "MN");
-        assertEquals(firstResponse.getShow().getNetwork().getCountry().getTimezone(), "Asia/Ulaanbaatar");
-        assertNull(firstResponse.getShow().getWebChannel());
-        assertNull(firstResponse.getShow().getExternals().getTvrage());
-        assertNull(firstResponse.getShow().getExternals().getThetvdb());
-        assertEquals(firstResponse.getShow().getExternals().getImdb(), "tt8709752");
-        assertEquals(firstResponse.getShow().getImage().getMedium(), "http://static.tvmaze.com/uploads/images/medium_portrait/191/478539.jpg");
-        assertEquals(firstResponse.getShow().getImage().getOriginal(), "http://static.tvmaze.com/uploads/images/original_untouched/191/478539.jpg");
-        assertNull(firstResponse.getShow().getSummary());
-        assertEquals(firstResponse.getShow().getUpdated(), (Integer) 1555040569);
-        assertEquals(firstResponse.getShow().getLinks().getSelf().getHref(), "http://api.tvmaze.com/shows/41734");
+
+        TVMazeApiResponse.Show show = firstResponse.getShow();
+        assertEquals(show.getId(), (Integer) 41734);
+        assertEquals(show.getUrl(), "http://www.tvmaze.com/shows/41734/girls");
+        assertEquals(show.getName(), "GIRLS");
+        assertEquals(show.getType(), "Scripted");
+        assertEquals(show.getLanguage(), "Mongolian");
+        assertEquals(show.getGenres(), singletonList("Comedy"));
+        assertEquals(show.getStatus(), "Running");
+        assertEquals(show.getRuntime(), (Integer) 41);
+        assertNull(show.getPremiered());
+        assertNull(show.getOfficialSite());
+        assertNull(show.getRating().getAverage());
+        assertEquals(show.getWeight(), (Integer) 0);
+        assertNull(show.getWebChannel());
+        assertNull(show.getSummary());
+        assertEquals(show.getUpdated(), (Integer) 1555040569);
+        assertEquals(show.getLinks().getSelf().getHref(), "http://api.tvmaze.com/shows/41734");
+
+        TVMazeApiResponse.Schedule schedule = show.getSchedule();
+        assertEquals(schedule.getTime(), "");
+        assertEquals(schedule.getDays(), singletonList("Thursday"));
+
+        TVMazeApiResponse.Network network = show.getNetwork();
+        assertEquals(network.getId(), (Integer) 1672);
+        assertEquals(network.getName(), "UBS");
+        assertEquals(network.getCountry().getName(), "Mongolia");
+        assertEquals(network.getCountry().getCode(), "MN");
+        assertEquals(network.getCountry().getTimezone(), "Asia/Ulaanbaatar");
+
+        TVMazeApiResponse.Externals externals = show.getExternals();
+        assertNull(externals.getTvrage());
+        assertNull(externals.getThetvdb());
+        assertEquals(externals.getImdb(), "tt8709752");
+
+        TVMazeApiResponse.Image image = show.getImage();
+        assertEquals(image.getMedium(), "http://static.tvmaze.com/uploads/images/medium_portrait/191/478539.jpg");
+        assertEquals(image.getOriginal(), "http://static.tvmaze.com/uploads/images/original_untouched/191/478539.jpg");
     }
 }
